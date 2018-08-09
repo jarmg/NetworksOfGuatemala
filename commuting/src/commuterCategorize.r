3892f7ff2a6be0ff9369fafecc89f0d2f9c1227c
Theodore Hadges <th2673@columbia.edu>
2018-08-08 20:08:14 -0600

    add group_by functionality

--- commuting/src/commuterCategorize.r
+++ commuting/src/commuterCategorize.r
@@ -1,33 +1,44 @@
-# sort by ANUMBER
-# for each ANUMBER:
-  # find which CELLID is closest at typical home time
-  # find which CELLID is closest at typical away time
+require(dplyr)
+
+# returns true if date falls on either a Saturday or Sunday. returns false otherwise
+# TODO:
+# output day of week instead 
+isWeekend <- function(x) {
+  if (weekdays(as.Date(x)) == "Saturday" | (weekdays(as.Date(x)) == "Sunday"))
+    return(TRUE)
+  else
+    return(FALSE)
+}
 
 # import call detail records into cdr
-cdrRaw <- read.csv("Projects/guatemala/raw_data/cdr.csv")
-cdrRaw # print to check
-
-# convert to data frame structure
-require(data.table) 
-cdr <- as.data.table(cdrRaw)
+cdr <- read.csv("Projects/guatemala/raw_data/cdr.csv")
+cdr # print to check
 
 
 
-#rename the columns
-names(cdr) <- c("startTime", "caller", "receiver", "seconds", "callFrom", "callTo", "network", "towerID") # name the columns
-cdr # print the data to look at it 
-#for each cdr[caller=="7IFFJJHOO"]
+# group by cell tower (CELL_ID) and number of times 
+group_by(cdr, CELL_ID) %>% summarise(num = n())
+
+# group by caller (ANUMBER) and show the mean duration of call
+group_by(cdr, ANUMBER) %>% summarise(meanNumSeconds = mean(SECONDS))
+
+# group by caller (ANUMBER) and say if the call was made on a weekend
+group_by(cdr, ANUMBER) %>% summarise(isWeekend = isWeekend(START_DATE_TIME))
 
 
-# drop unnecessary columns
-cdr$callFrom <- NULL
-cdr$callTo <- NULL
-cdr$network <- NULL
-cdr$receiver <- NULL
-cdr$seconds <- NULL
-cdr
+group_by(cdr, ANUMBER) %>% summarise_if(isnumeric, mean, na.rm = TRUE)
+#summarise_if(isWeekend(START_DATE_TIME), mean, na.rm = TRUE)
 
 
-# isolate caller
-cdr
+
+
+
+# group by caller (ANUMBER) and number of outgoing calls
+group_by(cdr, ANUMBER) %>% summarise(num = n())
+
+
+# class(weekdays(as.Date(cdr$START_DATE_TIME)))
+
+
+#group_by(cdr, ANUMBER, START_DATE_TIME)  %>% summarise(
 
