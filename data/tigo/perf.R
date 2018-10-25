8543e0bfa28f47a80a652b7dce2efa1846b83cb5
Jared Michael Greene <jaredgreene1@gmail.com>
2018-10-24 19:05:31 -0600

    Implement analysis for Prensa traffic
    Implement data cleaning and regression for analysis of
    Prensa traffic on ICT performance

--- data/tigo/perf.R
+++ data/tigo/perf.R
@@ -1,12 +1,42 @@
 library(dplyr)
 library(ggplot2)
 
-js <- readLines("export.json")
-data <- jsonlite::fromJSON(js)
+perf <- readLines("WQ_perfdata.json")
+perfData <- jsonlite::fromJSON(perf)
 
-plot.perfByDate <- function() {
-	data$fct_dt <- factor(data$fct_dt)
-	f <- group_by(data, fct_dt) %>%
+nav <- readLines("WQ_navdata.json")
+navData <- jsonlite::fromJSON(nav)
+
+
+
+
+getPrensaData <- function() {
+  traffic <- readLines("PrensaNavData.json") %>% jsonlite::fromJSON()
+  perf <- readLines("perfMaySept.json") %>% jsonlite::fromJSON()
+  
+  traffic$dateAndCell <- paste(traffic$`to_char(date_time,'yyyy-mm')`, traffic$bts_sh_nm)
+  perf$dateAndCell <- paste(perf$`to_char(fct_dt,'yyyy-mm')`, perf$lvl_val)
+  perf$perfRatio3g <- perf$`sum(timenavreq803g)`/perf$`sum(time3g)`
+  perf$perfRatio4g <- perf$`sum(timenavreq804g)`/perf$`sum(time4g)`
+  
+  md <- merge(perf, traffic, all = T)
+  
+  plot(md$perfRatio4g, md$`sum(bytes_down)`)
+  plot(md$perfRatio3g, md$`sum(bytes_down)`)
+  plot(md$perfRatio4g, md$`sum(subscribers)`)
+  plot(md$perfRatio4g, md$`sum(bytes_down)`/md$`sum(subscribers)`)
+  
+  summary(lm(md$`sum(bytes_down)`/md$`sum(subscribers)` ~ md$perfRatio3g))
+  
+  md$`sum(subscribers)`[is.na(md$`sum(subscribers)`)] <- 0
+  md$`sum(subscribers)`
+  summary(lm(md$`sum(subscribers)` ~ md$perfRatio4g))  
+}
+
+
+plot.perfByDate <- function(perf) {
+	perf$fct_dt <- factor(perf$fct_dt)
+	f <- group_by(perf, fct_dt) %>%
 		summarise(
 		  perf3g = mean(timenavreq803g/time3g, na.rm = T),
 		  perf4g = mean(timenavreq804g/time4g, na.rm = T),
@@ -19,13 +49,21 @@ plot.perfByDate <- function() {
 	  )
 }
 
-
-perfVariance(data) {
-  perf = data$perf3g
-  var(perf)  
+plot.navData <- function(navData) {
+  navData$service_name <- factor(navData$service_name)
+  gnd <- group_by(navData, service_name) %>% 
+    summarise(subs = sum(subscribers, na.rm = T))
+  plot(gnd)
+  ggplot() +
+    geom_line(
+      data = gnd, 
+      aes(x = date_time, 
+          y = subscribers,
+          color = "red"
+      )
+    )
 }
 
-
-plot.perfByDate()
+plot.perfByDate(perfData)
 
 
