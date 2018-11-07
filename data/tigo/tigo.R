b392f6551dcd36e8360990af66135f41bee9502e
Jared <jaredgreene1@gmail.com>
2018-11-07 10:21:45 -0600

    Rename data variables for increased clarity

--- data/tigo/tigo.R
+++ data/tigo/tigo.R
@@ -1,39 +1,83 @@
 library(dplyr)
 
+qosDataPath <- "~/Guatemala/NetworksOfGuatemala/data/tigo/perfMaySept.json"
+navDataPath <- "~/Guatemala/NetworksOfGuatemala/data/tigo/PrensaNavData.json"
+cellDataPath <- "~/Guatemala/NetworksOfGuatemala/data/tigo/cells.csv"
+
 addCellAverages <- function(prensa) {
-  callAvg <- group_by(prensa, bts_sh_nm) %>%
+  cellAvg <- group_by(prensa, bts_sh_nm) %>%
     summarise(
-           avgPerfRatio3g = mean(perfRatio3g, na.rm = T),
-           avgPerfRatio4g = mean(perfRatio4g, na.rm = T),
-           avgPrensaSubs = mean(`sum(subscribers)`, na.rm = T),
-           avgPrensaDown = mean(`sum(bytes_down)`, na.rm = T)
+           cellAvgPerfRatio3g = mean(perfRatio3g, na.rm = T),
+           cellAvgPerfRatio4g = mean(perfRatio4g, na.rm = T),
+           cellAvgPrensaSubs = mean(subscribers, na.rm = T),
+           cellAvgPrensaDown = mean(bytesdown, na.rm = T)
     )
 
-    left_join(prensa, callAvg) %>%
-      mutate(perfDiff4g = perfRatio4g - avgPerfRatio4g,
-              perfDiff3g = perfRatio3g - avgPerfRatio3g,
-              diffBytes = `sum(bytes_down)` - avgPrensaDown,
-              diffSubs =  `sum(subscribers)` - avgPrensaSubs)
+    left_join(prensa, cellAvg) %>%
+      mutate(cellPerfDiff4g   = perfRatio4g - cellAvgPerfRatio4g,
+              cellPerfDiff3g  = perfRatio3g - cellAvgPerfRatio3g,
+              cellBytesDiff   = bytesdown - cellAvgPrensaDown,
+              cellSubsDiff    = subscribers - cellAvgPrensaSubs)
 }
 
 
-mergeOnCellAndMonth <- function(traffic, perf) {
-  traffic$dateAndCell <- paste(traffic$`to_char(date_time,'yyyy-mm')`, traffic$bts_sh_nm)
-  perf$dateAndCell <- paste(perf$`to_char(fct_dt,'yyyy-mm')`, perf$lvl_val)
-  perf$perfRatio3g <- perf$`sum(timenavreq803g)`/perf$`sum(time3g)`
-  perf$perfRatio4g <- perf$`sum(timenavreq804g)`/perf$`sum(time4g)`
+aggregate.muni <- function(prensa) {
+  cityAvg <- group_by(prensa, CITY) %>%
+    summarise(
+           cityAvgPerfRatio3g = mean(perfRatio3g, na.rm = T),
+           cityAvgPerfRatio4g = mean(perfRatio4g, na.rm = T),
+           cityAvgPrensaSubs = mean(subscribers, na.rm = T)
+    )
+
+  prensa <- group_by(prensa, CITY, monthyear) %>%
+    summarise(
+           perfRatio3g = mean(perfRatio3g, na.rm = T),
+           perfRatio4g = mean(perfRatio4g, na.rm = T),
+           prensaSubs = sum(subscribers, na.rm = T)
+    )
+    
+    left_join(prensa, cityAvg) %>%
+      mutate(cityPerfDiff4g  = perfRatio4g - cityAvgPerfRatio4g,
+              cityPerfDiff3g = perfRatio3g - cityAvgPerfRatio3g,
+              citySubsDiff   = prensaSubs - cityAvgPrensaSubs)
+}
+
+
+
+
+aggregate.cellAndMonth <- function(traffic, perf) {
+  traffic$dateAndCell <- paste(traffic$monthyear, traffic$bts_sh_nm)
+  perf$dateAndCell <- paste(perf$monthyear, perf$lvl_val)
+  perf$perfRatio3g <- perf$timenavreq3g/perf$time3g
+  perf$perfRatio4g <- perf$timenavreq4g/perf$time4g
 
   merge(perf, traffic, all = F)
 }
 
 
-getPrensaData <- function() {
-  traffic <- readLines("PrensaNavData.json") %>% jsonlite::fromJSON()
-  perf <- readLines("perfMaySept.json") %>% jsonlite::fromJSON()
-  
-  d <- mergeOnCellAndMonth(traffic, perf)
+mergeLocationData <- function(prensa) {
+  cells <- read.csv(cellDataPath)
+  cells$cell <- cells$CELL_ID
+  left_join(prensa, cells)
+}
+
+
+get.prensa.cellData <- function() {
+  traffic <- readLines(navDataPath) %>% jsonlite::fromJSON()
+  perf <- readLines(qosDataPath)    %>% jsonlite::fromJSON()
+  d <- aggregate.cellAndMonth(traffic, perf)
+  d <- mergeLocationData(d)
   addCellAverages(d)
 }
   
+get.prensa.muniData <- function() {
+  traffic <- readLines(navDataPath) %>% jsonlite::fromJSON()
+  perf <- readLines(qosDataPath)    %>% jsonlite::fromJSON()
+  d <- aggregate.cellAndMonth(traffic, perf)
+  d <- mergeLocationData(d)
+  aggregate.muni(d)
+}
+  
+
 
 
