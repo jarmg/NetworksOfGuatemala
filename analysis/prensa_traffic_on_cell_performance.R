78a6e260ba4658180105c568f9666aea9c5fa385
Jared <jaredgreene1@gmail.com>
2018-11-08 08:21:10 -0600

    Implement analysis adjusting for regional diffs

--- analysis/prensa_traffic_on_cell_performance.R
+++ analysis/prensa_traffic_on_cell_performance.R
@@ -1,5 +1,25 @@
 source("../data/tigo/tigo.R")
 
+
+run.subsAndPerfByCelltoCell <- function(prensaData) {
+  # Relationship between variance from 5 month mean for each cell
+  # vs national average of variance for the month for # of prensa 
+  # subscribers and cell performance ratio 
+  
+  if (missing(prensaData)) { prensaData <- get.prensa.cellData() }
+
+  #collect average of the local monthly cell variance across the state
+  grouped <- group_by(prensaData, monthyear, STATE) %>%
+    summarise(stateSubsAvgVar  = mean(cellSubsDiff))
+
+  mutated <- left_join(prensaData, grouped) %>%
+    mutate(subShiftVar = cellSubsDiff - stateSubsAvgVar)
+  mutated
+  #plot(subShiftVar, cellPerfDiff4g)
+
+ } 
+
+
 run.subsAndPerfByCell <- function(prensaData, shouldPlot= F, net4g= T) {
   # Relationship between variance from 5 month mean for # of prensa 
   # subscribers and cell performance ratio 
